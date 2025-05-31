c575f0b3afdf5b6521ea2c800e56b368a451bda9
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2025-05-30 23:12:37 -0300

    criando header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-red-">Teste</Text>
+    <View className="w-full h-full flex justify-content items-center">
+      <Text className="text-xl text-red-500">Teste</Text>
     </View>
   );
 }
