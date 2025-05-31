130ff3c7ec66c434a15f136ecce8afa9682e086d
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2025-05-31 15:47:49 -0300

    header finalizado

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,4 +1,23 @@
-import { View } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { Pressable, Text, View } from "react-native";
 export default function Header() {
-  return <View>Teste</View>;
+  return (
+    <View className=" w-full flex flex-row items-center justify-between">
+      <Pressable className="w-10 h-10 bg-white  rounded-full flex justify-center items-center">
+        <Ionicons name="menu" size={20} color="#121212" />
+      </Pressable>
+
+      <View className="flex flex-col hustify-center items-center">
+        <Text className="text-center text-sm">Localização</Text>
+        <View className="flex-row justify-center items-center gap-1">
+          <Feather name="map-pin" size={14} color="#FF0000" />
+          <Text>Campo Grande</Text>
+        </View>
+      </View>
+
+      <Pressable className="w-10 h-10 bg-white  rounded-full flex justify-center items-center">
+        <Feather name="bell" size={20} color="#121212" />
+      </Pressable>
+    </View>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import Constants from "expo-constants";
+import { ScrollView, View } from "react-native";
+import Header from "./components/header";
+const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <View className="w-full h-full flex justify-content items-center">
-      <Text className="text-xl text-red-500">Teste</Text>
-    </View>
+    <ScrollView
+      style={{ flex: 1 }}
+      className="bg-slate-200"
+      showsVerticalScrollIndicator={false}
+    >
+      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
+        <Header />
+      </View>
+    </ScrollView>
   );
 }
